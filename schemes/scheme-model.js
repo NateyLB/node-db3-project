b9c57b09d7ae789e5da838c610a48bf319de404d
Nathan <nateyl.b@gmail.com>
2020-05-13 16:19:55 -0700

    added comments

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -9,15 +9,15 @@ module.exports={
      remove,
      addStep
 };
-//resolves to an array of all schemes
+//get all schemes, resolves to an array of all schemes
 function find(){
     return db("schemes")
 }
-//resolves to specified scheme
+//get shceme by shcema id, resolves to specified scheme
 function findById(id){
     return db("schemes").where({id}).first()
 }
-//resolves to all steps for specified scheme
+//find steps by schema id, resolves to all steps for specified scheme
 function findSteps(id){
     return db("schemes")
     .join("steps", "schemes.id", "=", "steps.scheme_id")
@@ -25,7 +25,7 @@ function findSteps(id){
     .where("scheme_id", id)
     .orderBy("step_number")
 }
-//added a new scheme to the scheme table
+//added a new scheme to the scheme table, resolves to the added scheme
 function add(scheme){
     return db("schemes")
     .insert(scheme, "id")
@@ -33,7 +33,7 @@ function add(scheme){
         return findById(ids[0]);
     })
 }
-//updates a specified scheme
+//updates a specified scheme, resolves to the updated scheme
 function update(changes, id){
     return db("schemes").where({id}).update(changes)
 }
@@ -41,7 +41,7 @@ function update(changes, id){
 function remove(id){
     return db("schemes").where({id}).del()
 }
-//adds a step
+//adds a step, resolves to the added step
 function addStep(step, scheme_id){
     return db("steps",)
     .insert(step, "id")
